8f8a8c2a92cbed606723c7a32fcdf4a6d090bbd2
IZanami77 <iZanamiNoOkami7@gmail.com>
2024-07-11 17:24:51 -0600

    Now can do basic operations and show it

--- script.js
+++ script.js
@@ -14,15 +14,87 @@ const btn9 = document.querySelector("#nine");
 const btn0 = document.querySelector("#btn0");
 const btnDivide = document.querySelector("#divide");
 const btnEqual = document.querySelector("#equal");
+const btnAc = document.querySelector("#ac");
 
 
-function add(num1,num2){
-    return num1 + num2;
-}
+num1 = "";
+num2 = "";
+element = "";
+result = ""
 
+btnAdd.addEventListener('click',() =>{
+    num1 = display.textContent;
+    num1 = parseInt(num1);
+    display.textContent = "";
+    element = "+";
+})
+
+btnSubs.addEventListener('click',() =>{
+    num1 = display.textContent;
+    num1 = parseInt(num1);
+    display.textContent = "";
+    element = "-";
+})
+
+btnPlus.addEventListener('click',() =>{
+    num1 = display.textContent;
+    num1 = parseInt(num1);
+    display.textContent = "";
+    element = "*";
+})
+btnDivide.addEventListener('click',() =>{
+
+    num1 = display.textContent;
+    num1 = parseInt(num1);
+    display.textContent = "";
+    element = "÷";
+})
+
+btnAc.addEventListener('click',()=>{
+    display.textContent = "0";
+    num1 = "";
+    num2 = "";
+    element = "";
+
+})
+
+btnEqual.addEventListener('click',()=>{
+    num2 = display.textContent;
+    num2 = parseInt(num2);
+    console.log(num2);
+
+    switch (element) {
+        case '+':
+            result = num1 + num2;
+            display.textContent = result
+            break;
+        case '-':
+            result = num1 - num2;
+            display.textContent = result
+            break;
+        case '*':
+            result = num1 * num2;
+            display.textContent = result
+            break;
+
+        case '÷':
+            if(num2 == 0){
+                alert("Cannot divide by zero dude")
+            }else{
+            result = num1 / num2;
+            display.textContent = result
+            }
+            break;
+    
+        default:
+            break;
+    }
+})
+
+const OPERATORS = ['+','-','*','÷'];
 
 btn1.addEventListener('click',() =>{
-    if(display.textContent == "0"){
+    if(display.textContent == "0" || OPERATORS.some(op => display.textContent.includes(op)) ){
         display.textContent = "1";
     }else{
     display.textContent += "1";
@@ -30,7 +102,7 @@ btn1.addEventListener('click',() =>{
 });
 
 btn2.addEventListener('click',() =>{
-    if(display.textContent == "0"){
+    if(display.textContent == "0" || OPERATORS.some(op => display.textContent.includes(op))){
         display.textContent = "2";
     }else{
     display.textContent += "2";
@@ -38,7 +110,7 @@ btn2.addEventListener('click',() =>{
 });
 
 btn3.addEventListener('click',() =>{
-    if(display.textContent == "0"){
+    if(display.textContent == "0" || OPERATORS.some(op => display.textContent.includes(op))){
         display.textContent = "3";
     }else{
     display.textContent += "3";
@@ -46,7 +118,7 @@ btn3.addEventListener('click',() =>{
 });
 
 btn4.addEventListener('click',() =>{
-    if(display.textContent == "0"){
+    if(display.textContent == "0" || OPERATORS.some(op => display.textContent.includes(op))){
         display.textContent = "4";
     }else{
     display.textContent += "4";
@@ -54,14 +126,14 @@ btn4.addEventListener('click',() =>{
 });
 
 btn5.addEventListener('click',() =>{
-    if(display.textContent == "0"){
+    if(display.textContent == "0" || OPERATORS.some(op => display.textContent.includes(op))){
         display.textContent = "5";
     }else{
     display.textContent += "5";
     }
 });
 btn6.addEventListener('click',() =>{
-    if(display.textContent == "0"){
+    if(display.textContent == "0" || OPERATORS.some(op => display.textContent.includes(op))){
         display.textContent = "6";
     }else{
     display.textContent += "6";
@@ -69,14 +141,14 @@ btn6.addEventListener('click',() =>{
 });
 
 btn7.addEventListener('click',() =>{
-    if(display.textContent == "0"){
+    if(display.textContent == "0" || OPERATORS.some(op => display.textContent.includes(op))){
         display.textContent = "7";
     }else{
     display.textContent += "7";
     }
 });
 btn8.addEventListener('click',() =>{
-    if(display.textContent == "0"){
+    if(display.textContent == "0" || OPERATORS.some(op => display.textContent.includes(op))){
         display.textContent = "8";
     }else{
     display.textContent += "8";
@@ -84,7 +156,7 @@ btn8.addEventListener('click',() =>{
 })
 
 btn9.addEventListener('click',() =>{
-    if(display.textContent == "0"){
+    if(display.textContent == "0" || OPERATORS.some(op => display.textContent.includes(op))){
         display.textContent = "9";
     }else{
     display.textContent += "9";
@@ -92,12 +164,8 @@ btn9.addEventListener('click',() =>{
 });
 
 btn0.addEventListener('click',() =>{
-    if(display.textContent != "0"){
+    if(display.textContent != "0" || OPERATORS.some(op => display.textContent.includes(op))){
         display.textContent += "0";
     }
 });
 
-btnAdd.addEventListener('click',() =>{
-        display.textContent += "+";
-    
-});
